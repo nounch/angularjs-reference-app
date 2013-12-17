6f40f8ae72ec64b99bdbf3fac6ee06058b922f3e
nounch <nounch@outlook.com>
2013-12-17 18:39:32 -0500

    Directive test
    Not functional. Problem: Directives cannot properly inspect/modify
    transcluded elements.

--- res/js/selfDocumenting.js
+++ res/js/selfDocumenting.js
@@ -1,17 +1,29 @@
 angular.module('selfDocumenting', [])
-  .directive('selfdocumenting', function() {
-    return  {
+  .directive('selfdocumenting', function($interpolate) {
+    return {
+      elementId: 0,
       restrict: 'E',
       transclude: true,
       scope: {},
-      controller: function($scope, $element) {
-        // $scope.elementsCount = $element;
+      // controller: function($scope, $element, $attrs, $transclude) {
+      // $scope.elementsCount = '<not available yet>';
+      // $scope.elementsCount = $element.parent().text();
+      // $scope.elementsCount = $interpolate($element.html())($scope);
+      // $scope.elementsCount = $interpolate('<div ng-transclude>{{3 + 3}}</div>')($scope);
+      // $scope.elementsCount = $transclude().text();
+      // $scope.elementsCount = $('.self-documenting-element');
+      // },
+      link: function($scope, $element, $attrs, controller, transcludeFn) {
+        //   // $scope.elementsCount = $element.find('li');
+        //   $scope.elementsCount = '<not available yet>';
+        // $scope.elementsCount = $('.self-documenting-element').find('ul').length;
+        this.elementId += self.elementId;  // XXX
+        $scope.elementsCount = $('.documented-element-' + self.elementId).html();
       },
-      link: function($scope, $element, attrs) {
-        // $scope.elementsCount = $element.find('li');
-        $scope.elementsCount = '<not available yet>';
+      template: function($element, $attrs) {
+        console.log('ID: ' + this.elementId);  // DEBUG
+        return '<div class="self-documenting"><div class="documentation"><p>Number of elements in this self-documenting element: <pre>{{elementsCount}}</pre></p></div><div class="documented-element-' + this.elelentId + '" ng-transclude></div></div>'
       },
-      template: '<div class="self-documenting"><div class="documentation"><p>Number of elements in this self-documenting element: {{elementsCount}}</p></div><div class="documented-element" ng-transclude></div></div>',
       replace: true
     }
   });
